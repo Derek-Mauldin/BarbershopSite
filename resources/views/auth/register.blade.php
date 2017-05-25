8f47a649d4e56dc166d7f152f82837b7baed429c
Derek Mauildin <derek.e.mauldin@gmail.com>
2017-05-24 22:12:08 -0600

    clean up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,9 +23,9 @@
 
 
             <div class="form-group">
-                <label for="shopName">Barbershop you work for</label>
+                <label for="shopName">Barbershop</label>
                 <select class="form-control" id="shopName" name="shopName" style="width: 40%">
-                    <option value="">Choose your barbershop</option>
+                    <option value="">Choose the shop the new administrator works for</option>
                     @foreach($barberShops as $shop)
                         <option value="{{ $shop->barbershop_name }}">{{ $shop->barbershop_name }}</option>
                     @endforeach
